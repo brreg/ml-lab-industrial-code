8d2ef4ce18bc8988fc339ec1b46a3f6f19c063bb
Stig B. Dørmænen <stigbd@gmail.com>
2018-09-05 12:01:00 +0200

    Fix server to match usage example

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
-def hello():
-    return "Hello MLAPI!"
+def predict():
+    data = [{'industrical code':'93.299', 'description':'Fritidsvirksomhet ellers' }]
+    return jsonify(data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
